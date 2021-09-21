88c440b22c9559f8a6d653cfe32500938cb67a31
kosrusal <64582812+kosrusal@users.noreply.github.com>
2021-09-21 22:52:08 +0300

    Add files via upload

---
+++ home_work_1/Kosolapov/Ex2.py
@@ -0,0 +1,6 @@
+x = float(input())
+y = float(input())
+if (x >= -2 and x <= 2) and (y <= 2 and y >= -2):
+    print("Да принадлежит")
+else:
+    print("Нет не принадлежит")
---
+++ home_work_1/Kosolapov/Ex5.py
@@ -0,0 +1,4 @@
+a = int(input())
+b = a + 1
+c = a - 1
+print(f"The next number for the number {a} is {b}. The previous number for the number {a} is {c}.")
