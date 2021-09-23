27b94734bfe2739a753e0da39c1c05e947c31615
Shifreee <90669730+Shifreee@users.noreply.github.com>
2021-09-23 17:16:02 +0300

    Add files via upload

---
+++ home_work_1/Titov/2.py
@@ -0,0 +1,6 @@
+x = int(input())
+y = int(input())
+if (x >= -2 and x <= 2) and (y >= -2 and y <= 2):
+    print('Принадлежит')
+else:
+    print('Не пренадлежит')
---
+++ home_work_1/Titov/5.py
@@ -0,0 +1,2 @@
+a = int(input())
+print('The next number for the number ' + str(a) + ' is ' + str(a + 1) + '. ' + 'The previous number for the number ' + str(a) + ' is ' + str(a - 1) + '.')
